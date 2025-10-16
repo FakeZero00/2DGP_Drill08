065f8e20913346024f98738d5eb0fe2e45619c5a
FakeZero(Laptop) <sjlimdaa@naver.com>
2025-10-16 18:03:47 +0900

    AutoRun 상태에서 화살표 누를시 Run 상태로 진입

--- boy.py
+++ boy.py
@@ -148,7 +148,7 @@ class Boy:
                 self.SLEEP : {space_down: self.IDLE},
                 self.IDLE : {a_down : self.AUTORUN, left_up : self.RUN, right_up : self.RUN, left_down : self.RUN, right_down : self.RUN, time_out: self.SLEEP},
                 self.RUN : {left_up: self.IDLE, right_up: self.IDLE, left_down: self.IDLE, right_down: self.IDLE},
-                self.AUTORUN : {time_out: self.IDLE}
+                self.AUTORUN : {left_down: self.RUN, right_down: self.RUN, time_out: self.IDLE}
             })
 
     def update(self):
